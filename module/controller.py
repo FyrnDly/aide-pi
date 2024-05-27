daf213320c21a8e32923ce51f6194b9ee0eb4f63
FyrnDly <fadramdani25@gmail.com>
2024-05-27 15:00:57 +0700

    [Create] Add Module Navigation and Controller Robot

---
+++ module/controller.py
@@ -0,0 +1,29 @@
+import time
+import pickle
+from module.navigation import get_current_state as state_model
+
+# Program navigation
+def navigation_robot(duration, agentModel, camera):
+    start_time = time.monotonic()
+    while True:
+        # Main Program Navigation
+        frame = camera.requestFrame(200)
+        if frame != None:
+            depth_buf = frame.getDepthData()
+            camera.releaseFrame(frame)
+            state = state_model(depth_buf)
+            # Choose action
+            action = agentModel.choose_action(state)
+            # Com serial
+            agentModel.get_com(action)
+            # Update Parameters
+            agentModel.update_parameters(state, action)
+        
+        # Check current time
+        current_time = time.monotonic()
+        elapsed_time = current_time - start_time
+        # Stop program when time out
+        if elapsed_time >= duration:
+            break
+    with open('model.pkl','wb') as model:
+        pickle.dump(agentModel,model)
